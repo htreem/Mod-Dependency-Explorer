5217f9b5e143af86d8840a8538b0b11180b7f17f
htreem <hoganj.ma@gmail.com>
2025-03-12 18:00:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import argparse
 from open_mods import open_mod_links
 from get_dependencies import get_all_dependencies
 
-parser = argparse.ArgumentParser()
-parser.add_argument("mod_id", nargs="?")
-args = parser.parse_args()
 
-if not args.mod_id:
-        args.mod_id = input("Enter Mod ID: ")  # Example: 3753 for Stardew Valley Expanded
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mod_id", nargs="?")
+    args = parser.parse_args()
 
-all_dependencies = get_all_dependencies(args.mod_id)
-print("All dependencies:", all_dependencies)
-open_mod_links(all_dependencies)
+    if not args.mod_id:
+            args.mod_id = input("Enter Mod ID: ")  # Example: 3753 for Stardew Valley Expanded
 
-
+    all_dependencies = get_all_dependencies(args.mod_id)
+    print("All dependencies:", all_dependencies)
+    open_mod_links(all_dependencies)
