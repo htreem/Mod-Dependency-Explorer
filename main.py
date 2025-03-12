d2a366f08df1bff683aacb9cfa1427854f3f0b87
htreem <hoganj.ma@gmail.com>
2025-03-12 17:59:22 +0000

    added argparse

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import argparse
 from open_mods import open_mod_links
 from get_dependencies import get_all_dependencies
 
-mod_id = input("Enter Mod ID: ")  # Example: 3753 for Stardew Valley Expanded
-all_dependencies = get_all_dependencies(mod_id)
+parser = argparse.ArgumentParser()
+parser.add_argument("mod_id", nargs="?")
+args = parser.parse_args()
+
+if not args.mod_id:
+        args.mod_id = input("Enter Mod ID: ")  # Example: 3753 for Stardew Valley Expanded
+
+all_dependencies = get_all_dependencies(args.mod_id)
 print("All dependencies:", all_dependencies)
 open_mod_links(all_dependencies)
+
 
